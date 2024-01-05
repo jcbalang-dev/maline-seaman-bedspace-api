50b0441d317b9887f51a414b4c7622ad4dbe7b50
John Christian Balang <dev.jcbalang@gmail.com>
2024-01-06 03:57:57 +0800

    add User Controller

---
+++ app/controllers/user_controller.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource, request
+from app.models.user_model import UserModel
+from app.views.user_view import UserView
+
+user_model = UserModel()
+
+class GetAllUserController(Resource):
+    def get(self):
+        users = user_model.get_all()
+        if users:
+            return UserView.render_users(users)
+        else:
+            return {'error', 'Data not exists'}, 404
+
+class GetUserIdController(Resource):
+    def get(self, id):
+        user = user_model.get(id)
+        if user:
+            return UserView.render_user(user)
+        else:
+            return {'error','User not found'}, 404
+                    
+class UserAuthController(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        user = user_model.auth_login(username, password)
+        if user:
+            return UserView.render_user(user)
+        else:
+            return {'error', 'Invalid Log In'}, 404
